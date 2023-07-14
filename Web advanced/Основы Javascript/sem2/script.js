6f07c1808641aefaf32cae753f17730388df37f1
Artem <xTema05x@mail.ru>
2023-07-14 21:44:31 +0300

    Знакомство с JavaScript

---
+++ Web advanced/Основы Javascript/sem2/script.js
@@ -0,0 +1,48 @@
+/* task1 */
+console.log("Text из script.js");
+
+// task2
+let num = 123;
+console.log(num);
+
+let a = 5;
+a = 7;
+console.log(a);
+
+let b = 1 + 2 + 3;
+console.log(b);
+
+// task3
+const c = 10;
+const d = 2;
+
+sum = c + d;
+diff = c - d;
+multi = c * d;
+div = c / d;
+
+console.log(sum, diff, multi, div);
+console.log(`all sum: ${sum + diff + multi + div}`);
+
+// task4
+const a = 1.5;
+const b = 0.75;
+console.log(`sum: ${a + b}`);
+
+const c = 5;
+console.log(`backside: ${c - c * 2}`);
+
+// task5
+let myName = {
+    firstName: "Artem",
+    lastName: "May",
+};
+console.log(`имя ${myName.firstName} фамилия ${myName.lastName}`);
+
+let java = "Java";
+let script = "Script";
+console.log(java + script);
+
+let hello = "Hello";
+let world = "world";
+console.log(hello + " " + world);
