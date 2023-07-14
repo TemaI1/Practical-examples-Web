eed7ecaa34fd99c3a308a27e4626c139524cf517
Artem <xTema05x@mail.ru>
2023-07-14 21:45:46 +0300

    add coment

--- Web advanced/Основы Javascript/sem2/script.js
+++ Web advanced/Основы Javascript/sem2/script.js
@@ -2,35 +2,35 @@
 console.log("Text из script.js");
 
 // task2
-let num = 123;
-console.log(num);
+// let num = 123;
+// console.log(num);
 
-let a = 5;
-a = 7;
-console.log(a);
+// let a = 5;
+// a = 7;
+// console.log(a);
 
-let b = 1 + 2 + 3;
-console.log(b);
+// let b = 1 + 2 + 3;
+// console.log(b);
 
-// task3
-const c = 10;
-const d = 2;
+// // task3
+// const c = 10;
+// const d = 2;
 
-sum = c + d;
-diff = c - d;
-multi = c * d;
-div = c / d;
+// sum = c + d;
+// diff = c - d;
+// multi = c * d;
+// div = c / d;
 
-console.log(sum, diff, multi, div);
-console.log(`all sum: ${sum + diff + multi + div}`);
+// console.log(sum, diff, multi, div);
+// console.log(`all sum: ${sum + diff + multi + div}`);
 
-// task4
-const a = 1.5;
-const b = 0.75;
-console.log(`sum: ${a + b}`);
+// // task4
+// const a = 1.5;
+// const b = 0.75;
+// console.log(`sum: ${a + b}`);
 
-const c = 5;
-console.log(`backside: ${c - c * 2}`);
+// const c = 5;
+// console.log(`backside: ${c - c * 2}`);
 
 // task5
 let myName = {
