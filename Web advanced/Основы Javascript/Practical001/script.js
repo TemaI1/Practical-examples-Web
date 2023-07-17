8a7cc414a5ac364f134690ff6afd30074cf7c712
Artem <xTema05x@mail.ru>
2023-07-17 12:28:17 +0300

    Знакомство с JavaScript

---
+++ Web advanced/Основы Javascript/Practical001/script.js
@@ -0,0 +1,11 @@
+let celsius = 33.8;
+let fahrenheit = (9 / 5) * celsius + 32;
+
+console.log(`Цельсия: ${celsius} Фаренгейта: ${fahrenheit}`);
+
+
+
+let name = "Artem";
+let admin = name;
+
+console.log(admin);
