d8b85d7923b7540a74f79a4a4cea0488b3b2085b
Artem <xTema05x@mail.ru>
2023-07-17 12:17:29 +0300

    Основы JavaScript

---
+++ Web advanced/Основы Javascript/sem3/script.js
@@ -0,0 +1,29 @@
+// alert("Hello");
+
+// prompt("Сколько вам лет?");
+
+// prompt("Сколько вам лет?", 20);
+
+// confirm("Дайте ответ!");
+
+
+
+// let name = prompt("Введите ваше имя");
+
+// if (name === "Artem") {
+//     alert("Привет Артем");
+// } else if (name === "Nastya") {
+//     alert("Привет Настя");
+// } else {
+//     alert(`Hello ${name}`);
+// }
+
+
+
+// let numFirst = prompt("Введите число");
+// let numSecond = prompt("Введите число с которым хотите сравнить");
+
+// (numFirst === numSecond) ? alert("Числа равны") : alert("Числа не равны");
+
+// alert((numFirst === numSecond) ? "Числа равны" : "Числа не равны");
+
