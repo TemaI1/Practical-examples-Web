526fe73aca8f48a909a1407a8906f3c61f14d417
Artem <xTema05x@mail.ru>
2023-07-20 16:29:01 +0300

    function

---
+++ Web advanced/Основы Javascript/sem5/script.js
@@ -0,0 +1,22 @@
+function funSum(a, b) {
+    return a + b;
+}
+
+console.log(funSum(3, 4));
+
+
+
+const funParam = (param) => {
+    console.log(param);
+}
+
+funParam(10);
+
+
+
+const funDiff = (a, b) => a - b;
+
+console.log(funDiff(8, 5));
+
+
+
