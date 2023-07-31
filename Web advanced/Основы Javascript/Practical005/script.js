af20cb8ab4d6babd3f9a4f275f76d257c795d9e6
Artem <xTema05x@mail.ru>
2023-07-31 13:43:15 +0300

    Объекты в JavaScript

---
+++ Web advanced/Основы Javascript/Practical005/script.js
@@ -0,0 +1,130 @@
+// task1
+
+const numbers = {
+    keyin1: 1,
+    keyin2: 2,
+    keyin3: 3,
+    keyin4: 4,
+    keyin5: 5,
+    keyin6: 6,
+    keyin7: 7,
+}
+
+for (const key in numbers) {
+    if (numbers[key] >= 3) {
+        console.log(numbers[key]);
+    }
+}
+
+// task2
+
+const post = {
+    author: "John", // вывести этот текст
+    postId: 23,
+    comments: [
+        {
+            userId: 10,
+            userName: "Alex",
+            text: "lorem ipsum",
+            rating: {
+                likes: 10,
+                dislikes: 2, // вывести это число
+            },
+        },
+        {
+            userId: 5, // вывести это число
+            userName: "Jane",
+            text: "lorem ipsum 2", // вывести этот текст
+            rating: {
+                likes: 3,
+                dislikes: 1,
+            },
+        },
+    ],
+};
+
+console.log(
+    post.author,
+    post.comments[0].rating.dislikes,
+    post.comments[1].userId,
+    post.comments[1].text
+);
+
+// task3
+
+const products = [
+    {
+        id: 3,
+        price: 200,
+    },
+    {
+        id: 4,
+        price: 900,
+    },
+    {
+        id: 1,
+        price: 1000,
+    },
+];
+
+products.forEach(element => element.price *= 0.85);
+products.forEach(element => console.log(element));
+
+// task4
+
+const product = [
+    {
+        id: 3,
+        price: 127,
+        photos: [
+            "1.jpg",
+            "2.jpg",
+        ],
+    },
+    {
+        id: 5,
+        price: 499,
+        photos: [],
+    },
+    {
+        id: 10,
+        price: 26,
+        photos: [
+            "3.jpg",
+        ],
+    },
+    {
+        id: 8,
+        price: 78,
+    },
+];
+
+product.filter((photo) => {
+    if (Array.isArray(photo.photos) && photo.photos.length) {
+        console.log(photo);
+    }
+});
+
+product.sort(function (a, b) {
+    if (a.price > b.price) {
+        return 1;
+    }
+    if (a.price < b.price) {
+        return -1;
+    }
+    return 0;
+});
+console.log(product);
+
+// task5
+
+const week = {};
+
+const en = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+const ru = ["понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"];
+
+for (let i = 0; i < en.length; i++) {
+    week[en[i]] = ru[i];
+}
+
+console.log(week);
