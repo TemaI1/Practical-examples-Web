8869753a265b0e780e343bffe21ec2a3022bfe44
Artem <xTema05x@mail.ru>
2023-09-04 13:29:16 +0300

    added code + js

---
+++ project/script.js
@@ -0,0 +1,7 @@
+
+const elHeadBot = document.querySelector('.head_bot');
+
+if (window.matchMedia("(max-width: 1024px)").matches) {
+    elHeadBot.insertBefore(elHeadBot.children[2], elHeadBot.children[1]);
+    document.querySelector('.head_bot_center_catalog_text').textContent = "Каталог";
+}
