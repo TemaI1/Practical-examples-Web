5b667e8bf6af68353fc46d3e4132953189ef8b92
Artem <xTema05x@mail.ru>
2023-09-10 12:47:15 +0300

    added code

---
+++ Web advanced/Продолжение JavaScript/Practical009/data.js
@@ -0,0 +1,26 @@
+export const dataCar = `[
+    {
+        "carMake": "BMW",
+        "modelCar": "X5",
+        "yearIssue": "2004",
+        "mileage": "125000",
+        "color": "black",
+        "tax": "24000",
+        "box": "automatic",
+        "driveUnit": "front",
+        "owners": "3",
+        "url": ""
+    },
+    {
+        "carMake": "Audi",
+        "modelCar": "A6",
+        "yearIssue": "2020",
+        "mileage": "11000",
+        "color": "white",
+        "tax": "32000",
+        "box": "automatic",
+        "driveUnit": "full",
+        "owners": "1",
+        "url": ""
+    }]
+    `;
---
+++ Web advanced/Продолжение JavaScript/Practical009/script.js
@@ -0,0 +1,4 @@
+import { dataCar } from "./data.js";
+
+const data = JSON.parse(dataCar);
+console.log(data);
