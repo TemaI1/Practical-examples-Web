ca91f85fe903652a86967a2e2aaf4ed5afa2f9d5
Artem <xTema05x@mail.ru>
2023-09-11 15:17:18 +0300

    Работа с JSON

--- Web advanced/Продолжение JavaScript/Practical009/data.js
+++ Web advanced/Продолжение JavaScript/Practical009/data.js
@@ -2,25 +2,61 @@ export const dataCar = `[
     {
         "carMake": "BMW",
         "modelCar": "X5",
-        "yearIssue": "2004",
-        "mileage": "125000",
-        "color": "black",
-        "tax": "24000",
-        "box": "automatic",
-        "driveUnit": "front",
-        "owners": "3",
-        "url": "https://i.pinimg.com/564x/cb/0f/7a/cb0f7a81b75ee924c32dbfa885efd573.jpg"
+        "yearIssue": "2021 год",
+        "mileage": "2500 км",
+        "color": "белый",
+        "tax": "21585 руб.",
+        "box": "автоматическая",
+        "driveUnit": "передний",
+        "owners": "1",
+        "url": "https://i.pinimg.com/564x/fa/bf/25/fabf25d661d641e7370837e6f539b092.jpg"
     },
     {
         "carMake": "Audi",
         "modelCar": "A6",
-        "yearIssue": "2020",
-        "mileage": "11000",
-        "color": "white",
-        "tax": "32000",
-        "box": "automatic",
-        "driveUnit": "full",
+        "yearIssue": "2020 год",
+        "mileage": "11000 км",
+        "color": "красный",
+        "tax": "24960 руб.",
+        "box": "автоматическая",
+        "driveUnit": "полный",
         "owners": "1",
-        "url": "https://i.pinimg.com/564x/21/2a/7f/212a7f516509f933a02a680a7e54b9eb.jpg"
-    }]
-    `;
+        "url": "https://i.pinimg.com/564x/fa/bf/25/fabf25d661d641e7370837e6f539b092.jpg"
+    },
+    {
+        "carMake": "LADA",
+        "modelCar": "2121",
+        "yearIssue": "2016 год",
+        "mileage": "68000 км",
+        "color": "черный",
+        "tax": "1570 руб.",
+        "box": "механическая",
+        "driveUnit": "передний",
+        "owners": "3",
+        "url": "https://i.pinimg.com/564x/fa/bf/25/fabf25d661d641e7370837e6f539b092.jpg"
+    },
+    {
+        "carMake": "Hyundai",
+        "modelCar": "Elantra",
+        "yearIssue": "2018 год",
+        "mileage": "84000 км",
+        "color": "черный",
+        "tax": "5250 руб.",
+        "box": "автоматическая",
+        "driveUnit": "передний",
+        "owners": "2",
+        "url": "https://i.pinimg.com/564x/fa/bf/25/fabf25d661d641e7370837e6f539b092.jpg"
+    },
+    {
+        "carMake": "Toyota",
+        "modelCar": "Camry",
+        "yearIssue": "2018 год",
+        "mileage": "62000 км",
+        "color": "белый",
+        "tax": "18675 руб.",
+        "box": "автоматическая",
+        "driveUnit": "передний",
+        "owners": "1",
+        "url": "https://i.pinimg.com/564x/fa/bf/25/fabf25d661d641e7370837e6f539b092.jpg"
+    }
+    ]`;
--- Web advanced/Продолжение JavaScript/Practical009/script.js
+++ Web advanced/Продолжение JavaScript/Practical009/script.js
@@ -8,41 +8,62 @@ const content = document.querySelector(".content");
 data.forEach(({ carMake, modelCar, yearIssue, mileage, color, tax, box, driveUnit, owners, url }) => {
     const boxContent = document.createElement("div");
     content.appendChild(boxContent);
+    boxContent.setAttribute('class', 'boxStyle');
+
+    const imgEl = document.createElement("img");
+    imgEl.src = url;
+    boxContent.appendChild(imgEl);
+    imgEl.setAttribute('class', 'img');
 
     const carMakeEl = document.createElement("h4");
     carMakeEl.textContent = carMake;
     boxContent.appendChild(carMakeEl);
 
-    const modelCarEl = document.createElement("h6");
+    const modelCarEl = document.createElement("p");
     modelCarEl.textContent = modelCar;
     boxContent.appendChild(modelCarEl);
+    modelCarEl.setAttribute('class', 'textStyle');
+    modelCarEl.textContent = `Модельный ряд: ${modelCar}`;
 
-    const yearIssueEl = document.createElement("h6");
+    const yearIssueEl = document.createElement("p");
     yearIssueEl.textContent = yearIssue;
     boxContent.appendChild(yearIssueEl);
+    yearIssueEl.setAttribute('class', 'textStyle');
+    yearIssueEl.textContent = `Год выпуска: ${yearIssue}`;
 
-    const mileageEl = document.createElement("h6");
+    const mileageEl = document.createElement("p");
     mileageEl.textContent = mileage;
     boxContent.appendChild(mileageEl);
+    mileageEl.setAttribute('class', 'textStyle');
+    mileageEl.textContent = `Пробег: ${mileage}`;
 
-    const colorEl = document.createElement("h6");
+    const colorEl = document.createElement("p");
     colorEl.textContent = color;
     boxContent.appendChild(colorEl);
+    colorEl.setAttribute('class', 'textStyle');
+    colorEl.textContent = `Цвет: ${color}`;
 
-    const taxEl = document.createElement("h6");
+    const taxEl = document.createElement("p");
     taxEl.textContent = tax;
     boxContent.appendChild(taxEl);
+    taxEl.setAttribute('class', 'textStyle');
+    taxEl.textContent = `Налог: ${tax}`;
 
-    const boxEl = document.createElement("h6");
+    const boxEl = document.createElement("p");
     boxEl.textContent = box;
     boxContent.appendChild(boxEl);
+    boxEl.setAttribute('class', 'textStyle');
+    boxEl.textContent = `Коробка: ${box}`;
 
-    const driveUnitEl = document.createElement("h6");
+    const driveUnitEl = document.createElement("p");
     driveUnitEl.textContent = driveUnit;
     boxContent.appendChild(driveUnitEl);
+    driveUnitEl.setAttribute('class', 'textStyle');
+    driveUnitEl.textContent = `Привод: ${driveUnit}`;
 
-    const ownersEl = document.createElement("h6");
+    const ownersEl = document.createElement("p");
     ownersEl.textContent = owners;
     boxContent.appendChild(ownersEl);
-
+    ownersEl.setAttribute('class', 'textStyle');
+    ownersEl.textContent = `Владельцы: ${owners}`;
 });
