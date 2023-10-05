5ef6d89c2cfed27fed8a24b52bedc0d19cf3d9ea
Artem <xTema05x@mail.ru>
2023-10-05 18:47:29 +0300

    added code

---
+++ Web advanced/JavaScript про ECMAScript/Practical006/script.js
@@ -0,0 +1,60 @@
+// ""Получение данных о пользователе""
+
+function getUserData(myId) {
+    fetch('https://api.github.com/users')
+        .then((response) => response.json())
+        .then((data) => {
+            let user = "";
+            for (let i = 0; i < data.length; i++) {
+                if (data[i].id === myId) {
+                    user = data[i];
+                    console.log(user);
+                };
+            }
+            if (user === "") {
+                console.log(`Пользователь с id ${myId} не найден`);
+            }
+        })
+};
+
+getUserData(2);
+getUserData(39);
+
+
+
+
+
+// ""Отправка данных на сервер""
+
+function saveUserData(myUser) {
+    fetch("https://api.github.com/users", {
+        method: "POST",
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(myUser)
+    })
+        .then(() => {
+            console.log('User data saved successfully');
+        })
+        .catch(error => console.error(error));
+}
+
+const user = {
+    name: 'John Smith',
+    age: 30,
+    email: 'john@example.com'
+};
+
+saveUserData(user);
+
+
+
+
+
+// ""Изменение стиля элемента через заданное время""
+
+function changeStyleDelayed(id, time) {
+    const elem = document.getElementById(id);
+    setTimeout(() => { elem.style.border = "2px solid red"; }, time);
+}
+
+changeStyleDelayed("myElement", 2000);
