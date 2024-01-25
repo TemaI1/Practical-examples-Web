53d0180cc877c2ad8cffe83ab7741742035afb8c
Artem <84618666+TemaI1@users.noreply.github.com>
2024-01-25 13:43:30 +0300

    смена контента (select, option, js method)

---
+++ Web advanced/Web solutions/001/script.js
@@ -0,0 +1,11 @@
+render = () => {
+	const items = document.querySelectorAll('.evidence-content');
+	const selector = document.querySelector('.evidence-select');
+	const index = +selector.value - 1;
+	for (let i = 0; i < items.length; i++) {
+		items[i].style.display = (i === index) ? 'block' : 'none';
+	}
+	selector.addEventListener('input', render);
+}
+
+render();
